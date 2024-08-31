ba8fcdb9a827594ad63512743276302fc05849fa
porchi <159401843+jorgealejandro1111@users.noreply.github.com>
2024-08-30 18:44:39 -0700

    Práctica 1 inicios en php 30/08/2024
    Castañeda Rivas Jorge Alejandro

---
+++ BasedeDatosWeb/parcial1/practica1.php
@@ -0,0 +1,24 @@
+<?php
+    $nombre = "Elporchi";
+
+    $numero = 13;
+
+    $numero2 = "13";
+
+    $total = $numero + $numero2;
+
+    echo "TOTAL = " . $total;
+
+    
+    echo "<h1> HOLA MUNDO </h1>";
+
+    for ($i=0; $i<=10; $i++){
+
+        echo $i . "<br>";
+    }
+    echo "<br>";
+
+    echo "<h2 style = 'color: red;' > <center> JORGE ALEJANDRO CASTAÑEDA RIVAS </center> </h2>";
+
+
+?> 
