b45b7214e8a1ad7c8d0ab5529c0e2204fa522b7f
porchi <159401843+jorgealejandro1111@users.noreply.github.com>
2024-09-11 19:06:18 -0700

    Practica #1 11/09/2024
    Castañeda Rivas Jorge Alejandro

---
+++ BasedeDatosWeb/parcial1/practica1.php
@@ -0,0 +1,30 @@
+<?php
+
+    $nombre = $_GET["nombre"];
+    $edad = $_GET["edad"];
+    $escuela = $_GET["escuela"];
+    $numero = $_GET["numero"];
+    $consola = $_GET["consola"];
+
+    //obtienen los valores
+
+    if($edad > 18){
+        echo "<h1>".$nombre." es mayor de edad</h1>";
+    } else {
+        echo "<h1>".$nombre." NO es mayor de edad</h1>";
+    }
+    
+    echo "<h1>Nombre del Alumno: ".$nombre."</h1>";
+
+    echo "<h2>Edad: ".$edad."</h2>";
+
+    echo "<h2>Escuela: ".$escuela."</h2>";
+
+    echo "<h2>Numero telefonico: ".$numero."</h2>";
+
+    echo "<h2>Consola favorita: ".$consola."</h2>";
+
+
+    //Imprimir el valor de esos campos
+
+?>
