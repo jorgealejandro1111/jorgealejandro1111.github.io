d8d5f5ce6bf96c0778c1adae2317c304e7317e94
porchi <159401843+jorgealejandro1111@users.noreply.github.com>
2024-10-16 18:28:15 -0700

    Actualización práctica #9.
    Castañeda Rivas Jorge Alejandro

--- BasedeDatosWeb/parcial2/practica9.js
+++ BasedeDatosWeb/parcial2/practica9.js
@@ -25,4 +25,37 @@ $(document).ready(function(){
         }
     });
 
+    cargarCartas();
+
+    function cargarCartas(){
+        var carta1 = 0;
+        var carta2 = 0;
+        let asignadas = new Array(54).fill(false);
+
+        while(carta1 < 16){
+            var num =Math.floor(Math.random() * 54)+1;
+            if(asignadas[num-1]== false){
+                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='imagenes/"+num+".jpg'></div>");
+                asignadas[num-1] = true; 
+                carta1++;
+            }else{
+                continue;
+            }
+        }
+
+        asignadas = new Array(54).fill(false);
+
+        while(carta2 < 16){
+            var num =Math.floor(Math.random() * 54)+1;
+            if(asignadas[num-1]== false){
+                $("#carta2").append("<div class='col-sm-3'><img class='img-fluid' src='imagenes/"+num+".jpg'></div>");
+                asignadas[num-1] = true; 
+                carta2++;
+            }else{
+                continue;
+            }
+        }
+        
+    }
+
 });
